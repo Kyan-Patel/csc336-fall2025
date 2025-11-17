805ee69bed482c411468db0d6f399cb10f2f259d
Kyan Patel <patelkyan@gmail.com>
2025-11-17 10:20:31 -0500

    Deleted React 9 Basics

--- 09-react-basics/src/components/ListItem.jsx
+++
@@ -1,8 +0,0 @@
-// src/components/ListItem.jsx
-export default function ListItem({ text, important }) {
-  return (
-    <li className={important ? "listitem listitem--important" : "listitem"}>
-      {text}
-    </li>
-  );
-}
